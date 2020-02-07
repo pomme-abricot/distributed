87940aa78d71b04ac596c7347e7305f6cb0ed10d
gaetan heidsieck <gaetan.heidsieck@inria.fr>
2020-02-07 09:50:14 +0100

    Add cache method: Greedy- FakeAdaptive

--- src/openalea/distributed/cache/cache_selection.py
+++ src/openalea/distributed/cache/cache_selection.py
@@ -1,5 +1,23 @@
 def cache_add_selection(node_metadata, cache_method):
-    return True
+    if cache_method == "adaptive":
+        # TODO: implement the selection for real
+        # For now: cache if the vid is: binarise(26), recons3D(9), skeletonize(35), segment_red(34)
+        vid = node_metadata['vid']
+        if (vid == 26) or (vid == 9) or (vid == 35) or (vid == 34):
+            return True 
+        else:
+            return False
 
-def cache_reuse_selection(node_metadata, cache_method):
-    return True
+    if cache_method == "greedy":
+        return True
+    
+    if cache_method == "None":
+        return False
+    # retrun False if cache method is unknown
+    return False
+
+def cache_reuse_selection(node_metadata, reuse_method):
+    if reuse_method == "None":
+        return False
+    else:
+        return True
