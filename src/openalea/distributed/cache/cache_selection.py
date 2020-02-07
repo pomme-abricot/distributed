d7ea996942c4422e74bdf9d4e72cea9433aee966
gaetan heidsieck <gaetan.heidsieck@inria.fr>
2020-02-07 15:41:05 +0100

    add reuse mehtod adaptive and greedy

--- src/openalea/distributed/cache/cache_selection.py
+++ src/openalea/distributed/cache/cache_selection.py
@@ -19,5 +19,15 @@ def cache_add_selection(node_metadata, cache_method):
 def cache_reuse_selection(node_metadata, reuse_method):
     if reuse_method == "None":
         return False
-    else:
+    if reuse_method == "greedy":
         return True
+    if reuse_method == "adaptive":
+        # TODO: implement the selection for real
+        # For now: cache if the vid is: binarise(26), recons3D(9), skeletonize(35), segment_red(34)
+        vid = node_metadata['vid']
+        if (vid == 26) or (vid == 9) or (vid == 35) or (vid == 34):
+            return True 
+        else:
+            return False
+    else:
+        return False
